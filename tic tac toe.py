9aba1a95103bff4783ce646206fc62d60f23b96f
ryder <ryderortiz7@gmail.com>
2025-04-10 21:48:29 -0400

    centered game window and added formatting

--- tic tac toe.py
+++ tic tac toe.py
@@ -16,6 +16,9 @@ background = "#D3D3D3"
 def set_tile(row, col):
     pass
 
+def new_game():
+    pass
+
 #setup the window
 window = tkinter.Tk()
 window.title("Tic Tac Toe Game")
@@ -25,7 +28,7 @@ frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text=current_player+"'s turn!", font=("Helvetica", 20), 
                       background=background, foreground="white")
 
-label.grid(row=0, column=0)
+label.grid(row=0, column=0, columnspan=3, sticky="we")
 
 for row in range(3):
     for col in range(3):
@@ -34,6 +37,23 @@ for row in range(3):
                                               width=4, height=1, command=lambda row=row, column=col: set_tile(row, col))
         game_board[row][col].grid(row=row+1, column=col)
 
+button = tkinter.Button(frame, text="restart", font=("Helvetica", 20), background = background,
+                        foreground="white", command=new_game)
+
+button.grid(row=4, column=0, columnspan=3, sticky="we")
+
 frame.pack()
+
+# center the game window
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+# format "(w)x(h)+x+y"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 window.mainloop()
