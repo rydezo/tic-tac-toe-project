90f2af314d326f0f720539005331971c5b287452
ryder <ryderortiz7@gmail.com>
2025-04-16 22:10:29 -0400

    Setup the game tiles, started checking for winner

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,8 +1,48 @@
 import random
 import tkinter #GUI library for the game
 
+def set_tile(row, col):
+    global current_player
+
+    if game_board[row][col]["text"] != "":
+        # if spot is already taken
+        return
+    
+    game_board[row][col]["text"] = current_player
+
+    # alternate the players
+    if current_player == o_player:
+        current_player = x_player
+    else:
+        current_player = o_player
+
+    label["text"] = current_player + "'s turn"
+
+    #check winning conditions
+    check_winner()
+
+def check_winner():
+    global turns
+    global game_over
+    turns +=1
+
+    #check horizontally
+    for row in range(3):
+        if (game_board[row][0]["text"] == game_board[row][1]["text"] 
+            == game_board[row][2]["text"] and game_board[row][0]["text"] != ""):
+            label.config(text=game_board[row][0]["text"] + " is the winner!", foreground=orange_color)
+            # add color effect to winning arrangement
+            for col in range(3):
+                game_board[row][col].config(foreground=orange_color, background=background)
+            game_over = True
+            return
+
+def new_game():
+    pass
+
+#setup players and board
 x_player = "X"
-y_player = "Y"
+o_player = "O"
 current_player = x_player
 
 game_board = [[0, 0, 0], 
@@ -10,14 +50,12 @@ game_board = [[0, 0, 0],
               [0, 0, 0]]
 
 blue_color = "#7393B3"
-orange_color = "#FFAC1C"
+orange_color = "#FF8C00"
 background = "#D3D3D3"
 
-def set_tile(row, col):
-    pass
-
-def new_game():
-    pass
+#game conditions
+turns = 0
+game_over = False
 
 #setup the window
 window = tkinter.Tk()
@@ -30,11 +68,12 @@ label = tkinter.Label(frame, text=current_player+"'s turn!", font=("Helvetica", 
 
 label.grid(row=0, column=0, columnspan=3, sticky="we")
 
+#setup each square
 for row in range(3):
     for col in range(3):
         game_board[row][col] = tkinter.Button(frame, text="", font=("Helvetica", 50, "bold"),
                                               background=background, foreground=blue_color,
-                                              width=4, height=1, command=lambda row=row, column=col: set_tile(row, col))
+                                              width=4, height=1, command=lambda row=row, col=col: set_tile(row, col))
         game_board[row][col].grid(row=row+1, column=col)
 
 button = tkinter.Button(frame, text="restart", font=("Helvetica", 20), background = background,
